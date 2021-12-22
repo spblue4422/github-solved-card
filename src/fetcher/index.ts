d4937ba79e3572ea1f498f2c60ca87cf1fefda8b
spblue4422 <spblue4422@naver.com>
2021-12-22 18:42:54 +0900

    파일 수정

--- src/fetcher/index.ts
+++ src/fetcher/index.ts
@@ -1,11 +1,14 @@
+import fetch from 'node-fetch';
+
 export const fetcher = async (username: string | string[]) => {
     try {
-        const data = await (
+        const data = (await (
             await fetch(`https://solved.ac/api/v3/user/show?handle=${username}`)
-        ).json();
+        ).json()) as any;
 
         return data;
     } catch (err) {
+        console.log(err);
         console.error('fetch Error');
     }
 };
