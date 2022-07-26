7a5863050aaac914c298f5c15b874857e6a4f2b6
spblue4422 <spblue4422@naver.com>
2022-07-26 17:04:49 +0900

    test1

--- api/index.ts
+++ api/index.ts
@@ -5,37 +5,37 @@ import { VercelRequest, VercelResponse } from '@vercel/node';
 
 export default async (req: VercelRequest, res: VercelResponse) => {
     const name: string | string[] = req.query['handle']; // spblue4422
-    try {
-        return res.send(name);
-    } catch (err) {
-        console.log(err);
-    }
     // try {
-    //     const width: number = 500;
-    //     const height: number = 150;
-
-    //     console.log(width);
-    //     console.log(height);
-
-    //     const rawData = await fetcher('spblue4422');
-
-    //     if (!rawData) throw new Error('no fetch');
-
-    //     const data: profile = {
-    //         handle: rawData.handle,
-    //         solvedCount: rawData.solvedCount,
-    //         tier: rawData.tier,
-    //         rating: rawData.rating,
-    //         rank: rawData.rank,
-    //         cls: rawData.cls
-    //     };
-
-    //     const newCard = new Card({ width, height, data });
-
-    //     return res.send(newCard.renderCard());
-    //     //return res.send(rawData);
+    //     return res.send(name);
     // } catch (err) {
-    //     console.error(err);
-    //     return res.status(404).send('err');
+    //     console.log(err);
     // }
+    try {
+        const width: number = 500;
+        const height: number = 150;
+
+        console.log(width);
+        console.log(height);
+
+        const rawData = await fetcher('spblue4422');
+
+        if (!rawData) throw new Error('no fetch');
+
+        // const data: profile = {
+        //     handle: rawData.handle,
+        //     solvedCount: rawData.solvedCount,
+        //     tier: rawData.tier,
+        //     rating: rawData.rating,
+        //     rank: rawData.rank,
+        //     cls: rawData.cls
+        // };
+
+        // const newCard = new Card({ width, height, data });
+
+        // return res.send(newCard.renderCard());
+        return res.send(rawData);
+    } catch (err) {
+        console.error(err);
+        return res.status(404).send('err');
+    }
 };
