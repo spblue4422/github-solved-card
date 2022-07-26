52b14e04bf7f8b7aba41ea42e9e584025c8560d7
spblue4422 <spblue4422@naver.com>
2022-07-26 17:16:11 +0900

    test5

--- src/card/index.ts
+++ src/card/index.ts
@@ -25,39 +25,40 @@ class Card {
 
         const { tierString, tierNumber } = await getTier(tier);
 
-        return `<svg 
-            width="${this.width}"
-            height="${this.height}"
-            viewBox = "0 0 ${this.width} ${this.height}"
-            fill= "none"
-            xmlns="http://www.w3.org/2000/svg">
-            <rect
-                x="0"
-                y="0"
-                width="${this.width} - 1"
-                height = "99%"
-                fille="#FFFFFF"
-                stroke="#424242"
-            />
-    
-            <g data-testid="card-tier">
-                <text x="10" y="5">${tierString}</text>
-                <foreignObject width="150" height="100" x="10" y="20">
-                    ${tiers[tier]}
-                </foreignObject>
-                <text x="10" y="125">${rating}</text>
-            </g>
+        return handle;
+        // return `<svg
+        //     width="${this.width}"
+        //     height="${this.height}"
+        //     viewBox = "0 0 ${this.width} ${this.height}"
+        //     fill= "none"
+        //     xmlns="http://www.w3.org/2000/svg">
+        //     <rect
+        //         x="0"
+        //         y="0"
+        //         width="${this.width} - 1"
+        //         height = "99%"
+        //         fille="#FFFFFF"
+        //         stroke="#424242"
+        //     />
 
-            <g data-testid="card-profile">
-                <text x="180" y="30">${handle}</text>
-                <foreignObject width="20" hegiht="20">
-                    ${classes[cls]}
-                </foreginObject>
-                <text x="180" y="60">Rank: ${rank}</text>
-                <text x="180" y="90">Solved: ${solvedCount}</text>
-            </g>
-        </svg>`;
-    };
+        //     <g data-testid="card-tier">
+        //         <text x="10" y="5">${tierString}</text>
+        //         <foreignObject width="150" height="100" x="10" y="20">
+        //             ${tiers[tier]}
+        //         </foreignObject>
+        //         <text x="10" y="125">${rating}</text>
+        //     </g>
+
+        //     <g data-testid="card-profile">
+        //         <text x="180" y="30">${handle}</text>
+        //         <foreignObject width="20" hegiht="20">
+        //             ${classes[cls]}
+        //         </foreginObject>
+        //         <text x="180" y="60">Rank: ${rank}</text>
+        //         <text x="180" y="90">Solved: ${solvedCount}</text>
+        //     </g>
+        // </svg>`;
+    }
 }
 
 export default Card;
