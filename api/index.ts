403c9d458274d4f56c2e8676a83505bca9068b67
spblue4422 <spblue4422@naver.com>
2022-07-26 16:37:59 +0900

    test

--- api/index.ts
+++ api/index.ts
@@ -5,33 +5,33 @@ import { VercelRequest, VercelResponse } from '@vercel/node';
 
 export default async (req: VercelRequest, res: VercelResponse) => {
     const name: string | string[] = req.query['handle']; // spblue4422
+    return res.send(name);
+    // try {
+    //     const width: number = 500;
+    //     const height: number = 150;
 
-    try {
-        const width: number = 500;
-        const height: number = 150;
+    //     console.log(width);
+    //     console.log(height);
 
-        console.log(width);
-        console.log(height);
+    //     const rawData = await fetcher('spblue4422');
 
-        const rawData = await fetcher('spblue4422');
+    //     if (!rawData) throw new Error('no fetch');
 
-        if (!rawData) throw new Error('no fetch');
+    //     const data: profile = {
+    //         handle: rawData.handle,
+    //         solvedCount: rawData.solvedCount,
+    //         tier: rawData.tier,
+    //         rating: rawData.rating,
+    //         rank: rawData.rank,
+    //         cls: rawData.cls
+    //     };
 
-        const data: profile = {
-            handle: rawData.handle,
-            solvedCount: rawData.solvedCount,
-            tier: rawData.tier,
-            rating: rawData.rating,
-            rank: rawData.rank,
-            cls: rawData.cls
-        };
+    //     const newCard = new Card({ width, height, data });
 
-        const newCard = new Card({ width, height, data });
-
-        return res.send(newCard.renderCard());
-        //return res.send(rawData);
-    } catch (err) {
-        console.error(err);
-        return res.status(404).send('err');
-    }
+    //     return res.send(newCard.renderCard());
+    //     //return res.send(rawData);
+    // } catch (err) {
+    //     console.error(err);
+    //     return res.status(404).send('err');
+    // }
 };
