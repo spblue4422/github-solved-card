2192060999ac11895e24e34241f7dcd258851dda
spblue4422 <spblue4422@naver.com>
2022-08-03 13:52:34 +0900

    test7

--- src/types/index.ts
+++ src/types/index.ts
@@ -7,14 +7,6 @@ export interface profile {
     cls: number;
 }
 
-export type level = 'Bronze' | 'Silver' | 'Gold' | 'Platinum' | 'Diamond' | 'Ruby' | 'Master';
+//export type level = 'Bronze' | 'Silver' | 'Gold' | 'Platinum' | 'Diamond' | 'Ruby' | 'Master';
 
-export const tierMap: Record<number, level> = {
-    0: 'Bronze',
-    1: 'Silver',
-    2: 'Gold',
-    3: 'Platinum',
-    4: 'Diamond',
-    5: 'Ruby',
-    6: 'Master'
-};
+export const tierMap = ['Bronze', 'Silver', 'Gold', 'Platinum', 'Diamond', 'Ruby', 'Master']
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,10 +1,10 @@
-import { level, tierMap } from '../types';
+import { tierMap } from '../types';
 
 export const getTier = async (tier: number) => {
     const quotient: number = await ((tier - 1) / 5);
     const remainder: number = await ((tier - 1) % 5); // 5가 0, 4가 1, 3이 2, 2가 3 1이 4
 
-    const tierString: level = tierMap[quotient];
+    const tierString: string = tierMap[quotient];
     const tierNumber: number = 5 - remainder;
 
     return { tierString, tierNumber }; //플래티넘만. 1,2,3,4,5는 굳이 반환 아해도됨.
